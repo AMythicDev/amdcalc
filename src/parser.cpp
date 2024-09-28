14d2a38e0b5e85bb9c6ed3f1740f3580aeb15b41
Arijit Dey <arijit79@protonmail.com>
2024-09-29 00:15:16 +0530

    feat: solve for mul and div

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,41 +1,93 @@
 #include "src/parser.h"
 #include "src/scanner.h"
 #include "src/solvetree.h"
-#include <stack>
 #include <vector>
+
+EvaluateNode **node_to_attach(EvaluateNode **node, EvaluateNode **root,
+                              OperationType op) {
+  if (*node == NULL)
+    return NULL;
+
+  if ((*node)->is_numeric_node()) {
+    return node_to_attach(&(*node)->parent_node, root, op);
+  }
+
+  OperationNode *node_to_check = dynamic_cast<OperationNode *>(*node);
+
+  if (static_cast<std::uint8_t>(op) <
+      static_cast<std::uint8_t>(node_to_check->op)) {
+    return node_to_attach(&(*node)->parent_node, root, op);
+  }
+
+  return node;
+}
+
+void attach_to_tree(EvaluateNode *node, EvaluateNode **curr_node,
+                    EvaluateNode **root, OperationType op) {
+  EvaluateNode **attach_node = node_to_attach(curr_node, root, op);
+
+  if (attach_node == NULL) {
+    node->lhs = *root;
+    *root = node;
+    return;
+  }
+
+  EvaluateNode *prev_rhs = (*attach_node)->rhs;
+
+  (*attach_node)->rhs = node;
+  node->lhs = prev_rhs;
+  node->parent_node = *attach_node;
+}
 
 SolverTree StreamingTokenParser::generate_tree() {
   std::vector<Token>::const_iterator iter = stream.begin();
 
   SolverTree st;
 
+  EvaluateNode **curr_node = NULL;
+
   for (; iter != stream.end(); iter++) {
     switch (iter->type) {
     case TokenType::Numeric: {
-      NumericNode *node = new NumericNode(iter->value.value());
+      EvaluateNode *node = new NumericNode(iter->value.value());
       if (st.root == NULL) {
         st.root = node;
       } else {
-        st.root->rhs = node;
+        (*curr_node)->rhs = node;
+        node->parent_node = *curr_node;
       }
+      curr_node = &node;
       break;
     }
     case TokenType::Add: {
-      OperationNode *node = new OperationNode(OperationType::Add);
-      node->lhs = st.root;
+      EvaluateNode *node = new OperationNode(OperationType::Add);
+      node->lhs = *curr_node;
       if (node->lhs == NULL) {
         node->lhs = new NumericNode(0);
       }
-      st.root = node;
+      attach_to_tree(node, curr_node, &st.root, OperationType::Sub);
+      curr_node = &node;
       break;
     }
     case TokenType::Subtract: {
-      OperationNode *node = new OperationNode(OperationType::Sub);
-      node->lhs = st.root;
+      EvaluateNode *node = new OperationNode(OperationType::Sub);
       if (node->lhs == NULL) {
         node->lhs = new NumericNode(0);
       }
-      st.root = node;
+      attach_to_tree(node, curr_node, &st.root, OperationType::Sub);
+      curr_node = &node;
+      break;
+    }
+    case TokenType::Multiply: {
+      EvaluateNode *node = new OperationNode(OperationType::Mul);
+      attach_to_tree(node, curr_node, &st.root, OperationType::Mul);
+      curr_node = &node;
+      break;
+    }
+    case TokenType::Divide: {
+      EvaluateNode *node = new OperationNode(OperationType::Div);
+      attach_to_tree(node, curr_node, &st.root, OperationType::Div);
+      curr_node = &node;
       break;
     }
     default:
--- src/solvetree.h
+++ src/solvetree.h
@@ -4,18 +4,21 @@
 #include <optional>
 
 enum class OperationType {
-  Sub,
-  Add,
-  Mul,
-  Div,
-  Exp,
+  Sub = 0,
+  Add = 1,
+  Mul = 2,
+  Div = 3,
+  Exp = 4,
 };
 
 class EvaluateNode {
 public:
-  virtual std::int32_t eval() = 0;
   EvaluateNode *lhs = NULL;
   EvaluateNode *rhs = NULL;
+  EvaluateNode *parent_node = NULL;
+
+  virtual std::int32_t eval() = 0;
+  virtual bool is_numeric_node() = 0;
 };
 
 class NumericNode : public EvaluateNode {
@@ -24,14 +27,16 @@ class NumericNode : public EvaluateNode {
 public:
   NumericNode(std::int32_t value) : value(value){};
   std::int32_t eval() { return value; }
+  bool is_numeric_node() { return true; }
 };
 
 class OperationNode : public EvaluateNode {
-  OperationType op;
-
 public:
+  OperationType op;
   OperationNode(OperationType op) : op(op){};
+
   std::int32_t eval();
+  bool is_numeric_node() { return false; }
 };
 
 class SolverTree {
