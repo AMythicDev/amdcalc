9cb481ca43cdd94531f3f1df7ccade973dfa17c1
Arijit Dey <arijit79@protonmail.com>
2024-09-30 01:13:56 +0530

    feat: parse single paren pair

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,7 @@
 #include "src/parser.h"
 #include "src/scanner.h"
 #include "src/solvetree.h"
+#include <stack>
 #include <vector>
 
 EvaluateNode **node_to_attach(EvaluateNode **node, EvaluateNode **root,
@@ -28,6 +29,10 @@ void attach_to_tree(EvaluateNode *node, EvaluateNode **curr_node,
 
   if (attach_node == NULL) {
     node->lhs = *root;
+
+    node->parent_node = (*root)->parent_node;
+    (*root)->parent_node = node;
+
     *root = node;
     return;
   }
@@ -37,6 +42,7 @@ void attach_to_tree(EvaluateNode *node, EvaluateNode **curr_node,
   (*attach_node)->rhs = node;
   node->lhs = prev_rhs;
   node->parent_node = *attach_node;
+  prev_rhs->parent_node = node;
 }
 
 SolverTree StreamingTokenParser::generate_tree() {
@@ -46,12 +52,19 @@ SolverTree StreamingTokenParser::generate_tree() {
 
   EvaluateNode **curr_node = NULL;
 
+  std::stack<EvaluateNode **> root_stack;
+
+  root_stack.push(&st.root);
+
+  EvaluateNode *subtree_ptr = NULL;
+
   for (; iter != stream.end(); iter++) {
     switch (iter->type) {
     case TokenType::Numeric: {
       EvaluateNode *node = new NumericNode(iter->value.value());
-      if (st.root == NULL) {
-        st.root = node;
+      if (*root_stack.top() == NULL) {
+        *root_stack.top() = node;
+        node->parent_node = subtree_ptr;
       } else {
         (*curr_node)->rhs = node;
         node->parent_node = *curr_node;
@@ -65,40 +78,57 @@ SolverTree StreamingTokenParser::generate_tree() {
       if (node->lhs == NULL) {
         node->lhs = new NumericNode(0);
       }
-      attach_to_tree(node, curr_node, &st.root, OperationType::Sub);
+      attach_to_tree(node, curr_node, root_stack.top(), OperationType::Sub);
       curr_node = &node;
       break;
     }
     case TokenType::Subtract: {
       EvaluateNode *node = new OperationNode(OperationType::Sub);
+      attach_to_tree(node, curr_node, root_stack.top(), OperationType::Sub);
       if (node->lhs == NULL) {
         node->lhs = new NumericNode(0);
       }
-      attach_to_tree(node, curr_node, &st.root, OperationType::Sub);
       curr_node = &node;
       break;
     }
     case TokenType::Multiply: {
       EvaluateNode *node = new OperationNode(OperationType::Mul);
-      attach_to_tree(node, curr_node, &st.root, OperationType::Mul);
+      attach_to_tree(node, curr_node, root_stack.top(), OperationType::Mul);
       curr_node = &node;
       break;
     }
     case TokenType::Divide: {
       EvaluateNode *node = new OperationNode(OperationType::Div);
-      attach_to_tree(node, curr_node, &st.root, OperationType::Div);
+      attach_to_tree(node, curr_node, root_stack.top(), OperationType::Div);
       curr_node = &node;
       break;
     }
     case TokenType::Exponent: {
       EvaluateNode *node = new OperationNode(OperationType::Exp);
-      attach_to_tree(node, curr_node, &st.root, OperationType::Exp);
+      attach_to_tree(node, curr_node, root_stack.top(), OperationType::Exp);
       curr_node = &node;
       break;
+    }
+    case TokenType::ParenOpen: {
+      if ((*curr_node)->is_numeric_node()) {
+        curr_node = &(*curr_node)->parent_node;
+      }
+      subtree_ptr = *curr_node;
+      root_stack.push(&(*curr_node)->rhs);
+      curr_node = &(*curr_node)->rhs;
+      continue;
+    }
+    case TokenType::ParenClose: {
+      if (root_stack.size() > 1) {
+        *curr_node = (*root_stack.top())->parent_node;
+        root_stack.pop();
+        break;
+      }
     }
     default:
       break;
     }
+    subtree_ptr = NULL;
   }
 
   return st;
