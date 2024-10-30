96dd3f62d5894874dabc55c0c3d7bb9430e49f27
Arijit Dey <arijit79@protonmail.com>
2024-10-30 23:29:47 +0530

    refactor: use muparser for mathematical solving

--- src/amdcalc.cpp
+++ src/amdcalc.cpp
@@ -1,22 +1,46 @@
-#include "src/parser.h"
-#include "src/scanner.h"
-#include "src/solvetree.h"
 #include <iostream>
+#include <muParser.h>
+
+class VariableManager {
+  double variable_storage[100];
+  uint8_t var_count = 0;
+
+public:
+  double *add_variable(const char *var_name) {
+    if (var_count > 99)
+      throw mu::ParserError("Variable buffer overflow.");
+    variable_storage[var_count] = 0;
+    var_count++;
+    return &variable_storage[var_count];
+  }
+
+  void reset() { var_count = 0; }
+
+  static double *add_variable_to_instance(const char *var_name, void *vm_ptr) {
+    VariableManager *vm = (VariableManager *)vm_ptr;
+    return vm->add_variable(var_name);
+  }
+};
 
 int main(int argc, char *argv[]) {
   char expression[1024];
+  mu::Parser parser;
+  VariableManager vm;
+  parser.SetVarFactory(&VariableManager::add_variable_to_instance, &vm);
   while (true) {
     std::cout << "Calc: ";
-    std::cin.getline(expression, 1023);
-    Scanner tl(expression);
-    std::vector<Token> token_list = tl.scan_tokens();
+    std::cin.getline(expression, 1024);
 
-    StreamingTokenParser stp = StreamingTokenParser(token_list);
-    SolverTree st = stp.generate_tree();
-
-    std::optional<Value> res = st.solve();
-
-    std::cout << res.value() << std::endl;
+    parser.SetExpr(expression);
+    double res;
+    try {
+      res = parser.Eval();
+    } catch (mu::Parser::exception_type &e) {
+      std::cout << '\n' << e.GetMsg() << std::endl;
+      return -1;
+    }
+    std::cout << res << std::endl;
+    vm.reset();
   }
 
   return 0;
